8bb635e88d8359ccb97fd51c030c597123b95587
wongband <bryanwong7788@gmail.com>
2018-11-22 20:06:48 -0800

    Added context scripts to manifest.json

---
+++ Assets/JS/playback.js
@@ -0,0 +1,14 @@
+// init();
+//
+// function init(){
+//   const play = document.getElementById("play");
+//
+//   play.addEventListener("click", () => {
+//     playback();
+//   });
+// }
+//
+// function playback(){
+//   document.getElementsByClassName('ytp-play-button')[0].click();
+// }
+console.log("uebueh");
